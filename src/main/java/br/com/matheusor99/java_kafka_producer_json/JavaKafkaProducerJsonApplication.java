a9d730f0cc8486a068fe46177ec87875b00f8eed
matheusor99 <matheusor99@gmail.com>
2025-02-19 19:47:11 -0300

    Project

---
+++ src/main/java/br/com/matheusor99/java_kafka_producer_json/JavaKafkaProducerJsonApplication.java
@@ -0,0 +1,13 @@
+package br.com.matheusor99.java_kafka_producer_json;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaKafkaProducerJsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaKafkaProducerJsonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/matheusor99/java_kafka_producer_json/JavaKafkaProducerJsonApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.matheusor99.java_kafka_producer_json;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaKafkaProducerJsonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
