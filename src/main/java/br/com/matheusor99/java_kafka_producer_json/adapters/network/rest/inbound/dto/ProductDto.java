ad22e280e8cb67223c50d8d9a9350d4141f80126
matheusor99 <matheusor99@gmail.com>
2025-02-19 21:28:10 -0300

    removendo import não utilizado

--- src/main/java/br/com/matheusor99/java_kafka_producer_json/adapters/network/rest/inbound/dto/ProductDto.java
+++ src/main/java/br/com/matheusor99/java_kafka_producer_json/adapters/network/rest/inbound/dto/ProductDto.java
@@ -1,6 +1,4 @@
 package br.com.matheusor99.java_kafka_producer_json.adapters.network.rest.inbound.dto;
 
-import lombok.extern.jackson.Jacksonized;
-
 public record ProductDto(String name, String description, Double price) {
 }
